29190c274a887de43c8bba933e3664ba22f70730
HaiNguyen-Sotatek <hai.nguyen@sotatek.com>
2022-06-11 16:09:23 +0700

    remove unuse

--- src/main/MainScreen.tsx
+++ src/main/MainScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import styles from './MainStyle';
 import {View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
@@ -8,22 +8,9 @@ import Toast from 'react-native-simple-toast';
 import {useDispatch, useSelector} from 'react-redux';
 import {clearNetworkFail} from '../actions';
 import {NetworkState} from '../reducers';
-import {apolloClient} from '../api';
-import {LOG_IN_WITH_EMAIL} from '../globalGraphql';
-import {FetchResult} from 'apollo-boost';
-import {getToken, saveToken} from '../utils/localStorage';
 import AuthScreen from '../auth/AuthScreen';
 
 const Stack = createStackNavigator();
-
-interface LoginWithEmail {
-  loginWithEmail?: UserInfo;
-}
-
-interface UserInfo {
-  accessToken: string;
-  refreshToken: string;
-}
 
 const MainScreen = () => {
   const sendNetworkFail = useSelector((state: NetworkState) => {
@@ -31,26 +18,6 @@ const MainScreen = () => {
   });
   const dispatch = useDispatch();
   const clearNetworkStatus = () => dispatch(clearNetworkFail());
-
-  useEffect(() => {
-    logInWithEmailAction();
-  }, []);
-
-  const logInWithEmailAction = async () => {
-    try {
-      const token = await getToken();
-      if (!token) {
-        const result: FetchResult<LoginWithEmail> = await apolloClient.mutate({
-          mutation: LOG_IN_WITH_EMAIL,
-          variables: {email: 'john@doe.com', password: 'p4SSW0rd'},
-        });
-        const accessToken = result.data?.loginWithEmail?.accessToken ?? '';
-        saveToken(accessToken);
-      }
-    } catch (error) {
-      console.log('error', error);
-    }
-  };
 
   if (sendNetworkFail.err) {
     switch (sendNetworkFail.err) {
